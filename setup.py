74835e5331ddf741f72ead42e7ec4bcaa7e2e51d
Jeff Huth <jeff.huth@bytecode.io>
2019-11-04 16:52:53 -0800

    v0.0.3 clicks and impressions as integers
    Change data types for impressions and clicks in performance reports from number to integer.

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-google-search-console',
-      version='0.0.2',
+      version='0.0.3',
       description='Singer.io tap for extracting data from the Google Search Console API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_google_search_console'],
       install_requires=[
           'backoff==1.8.0',
-          'requests==2.22.0',
-          'singer-python==5.8.1'
+          'requests==2.20.0',
+          'singer-python==5.8.0'
       ],
       entry_points='''
           [console_scripts]
