b9eb3667d0c2856ea3044a67cc4e10c083c38186
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2021-05-28 17:46:08 +0530

    TDL-6750: Move test from tap-tester, increase coverage and best practices

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-google-search-console',
       install_requires=[
           'backoff==1.8.0',
           'requests==2.22.0',
-          'singer-python==5.8.1'
+          'singer-python==5.12.1'
       ],
       extras_require={
           'dev': [
