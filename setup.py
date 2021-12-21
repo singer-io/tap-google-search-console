afaf94a4d9d8be118c2d349ff434a1c9395da3fa
KrisPersonal <66801357+KrisPersonal@users.noreply.github.com>
2021-12-21 08:21:31 -0800

    Bump version for Request Timeout (#28)
    Co-authored-by: KrishnanG <kgurusamy@talend.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-google-search-console',
-      version='0.1.0',
+      version='0.1.1',
       description='Singer.io tap for extracting data from the Google Search Console API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
