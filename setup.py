371da3ae812ea88dfd3f1232df7bbd7da4e528e3
Leslie VanDeMark <38043390+leslievandemark@users.noreply.github.com>
2024-01-23 16:46:51 -0500

    Updates for py 3.11 (#43)
    * test py 3.11
    
    * keep on standard base image
    
    -----------------------------

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-google-search-console",
-    version="1.0.0",
+    version="1.1.0",
     description="Singer.io tap for extracting data from the Google Search Console API",
     author="jeff.huth@bytecode.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_google_search_console"],
-    install_requires=["backoff==1.8.0", "requests==2.22.0", "singer-python==5.13.0"],
+    install_requires=["backoff==2.2.1", "requests==2.31.0", "singer-python==6.0.0"],
     extras_require={
         "dev": [
             "ipdb",
