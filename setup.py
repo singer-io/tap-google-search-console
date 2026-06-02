830e74027aa834f1ec9472a5cacc13b72009865d
Leslie VanDeMark <lvandemark@talend.com>
2026-06-02 18:06:03 +0000

    bump requests library version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="tap-google-search-console",
-    version="1.1.1",
+    version="1.1.2",
     description="Singer.io tap for extracting data from the Google Search Console API",
     author="jeff.huth@bytecode.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_google_search_console"],
-    install_requires=["backoff==2.2.1", "requests==2.32.4", "singer-python==6.0.1"],
+    install_requires=["backoff==2.2.1", "requests==2.33.0", "singer-python==6.0.1"],
     extras_require={
         "dev": [
             "ipdb",
